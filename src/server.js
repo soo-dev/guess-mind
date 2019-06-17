40d1edbe211a8ec596f8af282d3ec1711f9839dc
sooyoungs0120@gmail.com <sooyoungs0120@gmail.com>
2019-06-18 00:17:33 +0900

    setup_socketio

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import { join } from "path";
 import socketIO from "socket.io";
 
-const PORT = 4000;
+const PORT = 3000;
 const app = express();
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
@@ -13,4 +13,7 @@ app.get("/", (req, res) => res.render("home"));
 const handleListening = () =>
   console.log(`Server running: http://localhost:${PORT} ✔`);
 
-app.listen(PORT, handleListening);
+const server = app.listen(PORT, handleListening);
+
+// http://localhost:3000/socket.io/socket.io.js 로 제대로 돼있는지 확인
+const io = socketIO(server);
