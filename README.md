47603c0dcbb344adf9ef584bd3cb22f712a2e509
sooyoungs0120@gmail.com <sooyoungs0120@gmail.com>
2019-06-17 23:29:10 +0900

    setup_server

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # guess-mind
 실시간 채팅 게임(SocketIO, Gulp, NodeJS)
+
+## To Do
+ - [x] Server
+ - [x] Pug
+ - [ ] Static
+ - [ ] SocketIO
--- src/server.js
+++ src/server.js
@@ -1,1 +1,17 @@
 import express from "express";
+import {
+    join
+} from "path";
+import socketIO from "socket.io";
+
+const PORT = 4000;
+const app = express();
+app.set('view engine', 'pug');
+app.set("views", join(__dirname, "views"));
+app.use(express.static(join(__dirname, "static")));
+
+app.get("/", (req, res) => res.render("home"));
+
+const handleListening = () => console.log(`Server running: http://localhost:${PORT} ✔`);
+
+app.listen(PORT, handleListening);
