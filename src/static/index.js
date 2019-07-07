4a020cf71126324d4c50e9a549ae96ed3d1ab2f5
devs <sooyoungs0120@gmail.com>
2019-07-07 23:32:10 +0900

    소켓 콘솔에서 다뤄봤음

--- src/static/index.js
+++ src/static/index.js
@@ -1,1 +1,22 @@
-hello
+// eslint-disable-next-line no-undef
+const socket = io("/");
+
+const sendMessage = message => {
+  socket.emit("newMessage", {
+    message
+  });
+  console.log(`You: ${message}`);
+};
+
+const setNickname = nickname => {
+  socket.emit("setNickname", {
+    nickname
+  });
+};
+
+const handleMessageNotif = data => {
+  const { message, nickname } = data;
+  console.log(`${nickname}: ${message}`);
+};
+
+socket.on("messageNotif", handleMessageNotif);
